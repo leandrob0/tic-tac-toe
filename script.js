7f1766074fe2a458f2749b4dcbc5d0385c1eba3b
Leandro Bovino <leandro.bovino@gmail.com>
2021-06-15 19:53:17 -0300

    visuals for marks displaying in each div added

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ createGrid();
 const myBoard = (function() {
     
     let gameBoard = [
-        ["X","O","X"],
-        ["X","O","X"],
-        ["O","X","O"]
+        ["","",""],
+        ["","",""],
+        ["","",""]
     ];
 
     function showGameBoard() {
@@ -40,20 +40,30 @@ const myBoard = (function() {
     }
 
     function returnDiv(e) {
-        console.log(e.target);
-        return e.target;
+        let boardPosition = e.target.id.split("");
+
+        gameBoard[boardPosition[0]][boardPosition[1]] = Player1.getMark();
+        showGameBoard();
     }
 
     function addFunctionality() {
         const divs = document.querySelectorAll(".game-space");
-        Array.from(divs).forEach(div => div.addEventListener("click", returnDiv));
+        let arrayDivs = Array.from(divs);
+        let k = 0;
+        for(let i = 0; i < 3 ; i++) {
+            for(let j = 0; j < 3; j++) {
+                arrayDivs[k].setAttribute("id",i.toString() + j.toString());
+                k++;
+            }
+        }
+        arrayDivs.forEach(div => div.addEventListener("click", returnDiv));
     }
 
-    return {gameBoard, showGameBoard};
+    return {gameBoard, showGameBoard, addFunctionality};
    
 })();
 
-//CONTROLS THE GAME FLOW ########################################
+/*
 const controlTheGame = (function() {
     function checkWinner(board) {
         myBoard.gameBoard; // se lo paso por parametro.
@@ -80,20 +90,61 @@ const controlTheGame = (function() {
             }
         }
     }
-})
+
+    return{checkSpaceAvailable};
+});
+*/
 
 //Player object ########################################
 const playerFactory = (name, mark) => {
+    this.mark = mark;
+    this.name = name;
     const getName  = () => name;
     const getMark = () => mark;
+
 
     return {getName , getMark};
 };
 
 myBoard.showGameBoard();
-  
-const Player1 = playerFactory("Player 1", "X");
 
-//EVENT LISTENER FOR THE MARK SELECTION
-const marksButtons = document.querySelectorAll(".player-mark");
-let arrayButtons = Array.from(marksButtons);
+let Player1 = playerFactory("Player 1",""); //I couldn't figure out how to update the mark value when this is a const from the factory function.
+let Player2 = playerFactory("Player 2","");//So i made it a let variable 😧 (yes i used an emoji in code)
+
+/*
+    ################################################
+
+        THE LOGIC FOR WHEN THE GAMES START.
+
+    ################################################
+*/
+function getRadioValues() {
+    var getMarkPlayer2 = document.querySelector('input[name="radioMark"]:checked');
+    var getMarkPlayer1 = document.querySelector('input[name="radioMark1"]:checked');   
+        
+    if(getMarkPlayer1 != null && getMarkPlayer2 != null) {   
+        if(getMarkPlayer1.value != getMarkPlayer2.value) {
+            Player1 = playerFactory("Player 1", getMarkPlayer1.value);
+            Player2 = playerFactory("Player 2", getMarkPlayer2.value);
+            console.log(Player1.getMark());
+        } else {
+            alert("The selected mark are the same for both players");
+        }
+    }   
+    else {   
+        alert("Error.");  
+    }   
+}
+
+function disableInputs() {
+    var getInputsP2 = document.querySelectorAll('input[name="radioMark"]');
+    var getInputsP1 = document.querySelectorAll('input[name="radioMark1"]'); 
+
+    Array.from(getInputsP2).forEach(element => element.disabled = true);
+    Array.from(getInputsP1).forEach(element => element.disabled = true);
+}
+
+const startGame = document.querySelector("#start-button");
+startGame.addEventListener("click", getRadioValues);
+startGame.addEventListener("click", disableInputs);
+startGame.addEventListener("click", myBoard.addFunctionality());
