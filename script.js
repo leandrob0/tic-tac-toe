cdec5a0efb7c2a72432db60a3ae0359dd5c7d559
Leandro Bovino <leandro.bovino@gmail.com>
2021-06-16 19:16:54 -0300

    game functionality added with no visuals for the game finish yet

--- script.js
+++ script.js
@@ -24,11 +24,12 @@ const myBoard = (function() {
         ["","",""]
     ];
 
+    let playerTurn = 1;
+
     function showGameBoard() {
         const divs = document.querySelectorAll(".game-space");
         let i = 0;
         let j = 0;
-        console.log(Array.from(divs));
         Array.from(divs).forEach(div => {
             if(j == 3) {
                 j = 0;
@@ -39,11 +40,36 @@ const myBoard = (function() {
         });
     }
 
+    //Function that takes care of all the game functionality
     function returnDiv(e) {
         let boardPosition = e.target.id.split("");
 
-        gameBoard[boardPosition[0]][boardPosition[1]] = Player1.getMark();
+        let playerWon = 0;
+
+        if(game.checkSpaceAvailable(boardPosition, gameBoard) == 1){return;}
+
+        //FILLS THE ARRAY
+        if(playerTurn == 1) {
+            gameBoard[boardPosition[0]][boardPosition[1]] = Player1.getMark();
+            playerTurn = 2;
+        } else {
+            gameBoard[boardPosition[0]][boardPosition[1]] = Player2.getMark();
+            playerTurn = 1;
+        }
+
         showGameBoard();
+
+        (game.checkWinner(gameBoard) == 1) ? playerWon = 1 : 
+        (game.checkWinner(gameBoard) == 2) ? playerWon = 2 : playerWon = 0;
+        
+        if(game.announceWinner(playerWon) === true) {
+            removeFunctionality();
+        }
+
+        if(game.checkIfFull(gameBoard) == 1 && (playerWon !== 1 || playerWon !== 2)) {
+            console.log("draw");
+            removeFunctionality();
+        }
     }
 
     function addFunctionality() {
@@ -59,41 +85,124 @@ const myBoard = (function() {
         arrayDivs.forEach(div => div.addEventListener("click", returnDiv));
     }
 
+    function removeFunctionality() {
+        const divs = document.querySelectorAll(".game-space");
+        let arrayDivs = Array.from(divs);
+        arrayDivs.forEach(div => div.removeEventListener("click", returnDiv));
+    }
+
     return {gameBoard, showGameBoard, addFunctionality};
    
 })();
 
-/*
-const controlTheGame = (function() {
-    function checkWinner(board) {
-        myBoard.gameBoard; // se lo paso por parametro.
+
+
+
+//Game flow #########################################
+const game = (function() {
+
+    function checkIfFull(board) {
+        let check = true;
+        let returned;
+        for(let i = 0; i < 3; i++) {
+            for(let j = 0; j < 3; j++) {
+                if(board[i][j] == "") {
+                    check = false;
+                } 
+            }
+        }
+        check ? returned = 1 : returned = 0;
+
+        return returned;
     } 
 
-    function checkSpaceAvailable(position,board) {
-        if(position < 3) {
-            if(board[0][position] == "") {
+    function checkWinner(board) {
+        let arrayResult = [];
+
+        //match diagonal
+        for(let i = 0; i < 3; i++) {
+            arrayResult.push(board[i][i]);
+        }
+        const allEqual = arr => arr.every(v => v === arr[0] && v !== "");
+        if(allEqual(arrayResult)) {
+            if(arrayResult[0] == Player1.getMark) {
                 return 1;
             } else {
-                return 0;
+                return 2;
             }
-        } else if(position > 2 && position < 6) {
-            if(board[1][position - 3] == "") {
+        }
+
+        arrayResult = [];
+
+        //match reverse diagonal
+        let counter = 2;
+        for(let i = 0; i < 3; i++) {
+            arrayResult.push(board[i][counter]);
+            counter--;
+        }
+        if(allEqual(arrayResult)) {
+            if(arrayResult[0] == Player1.getMark) {
                 return 1;
             } else {
-                return 0;
+                return 2;
             }
-        } else if(position > 5) {
-            if(board[2][position - 6] == "") {
-                return 1;
-            } else {
-                return 0;
+        }
+
+        arrayResult = [];
+
+        //match vertical line
+        for(let i = 0; i < 3; i++) {
+            for(let j = 0; j < 3; j++) {
+                arrayResult.push(board[j][i]);
             }
+            if(allEqual(arrayResult)) {
+                if(arrayResult[0] == Player1.getMark()) {
+                    return 1;
+                } else if(arrayResult[0] == Player2.getMark()){
+                    return 2;
+                }
+            }
+            arrayResult = [];
+        }
+
+        //match horizontal line
+        for(let i = 0; i < 3; i++) {
+            for(let j = 0; j < 3; j++) {
+                arrayResult.push(board[i][j]);
+            }
+            if(allEqual(arrayResult)) {
+                if(arrayResult[0] == Player1.getMark()) {
+                    return 1;
+                } else if(arrayResult[0] == Player2.getMark()){
+                    return 2;
+                }
+            }
+            arrayResult = [];
         }
     }
 
-    return{checkSpaceAvailable};
-});
-*/
+    function announceWinner(value) {
+        if(value == 1) {
+            console.log("Player 1 won");
+            return true;
+        } else if(value == 2) {
+            console.log("Player 2 won");
+            return true;
+        }
+
+        return false;
+    }
+
+    function checkSpaceAvailable(position,board) {
+        if(board[position[0]][position[1]] != "") {
+            return 1;
+        }
+    }
+
+    return {announceWinner, checkWinner, checkIfFull, checkSpaceAvailable};
+})();
+
+
 
 //Player object ########################################
 const playerFactory = (name, mark) => {
@@ -102,14 +211,16 @@ const playerFactory = (name, mark) => {
     const getName  = () => name;
     const getMark = () => mark;
 
-
     return {getName , getMark};
 };
+
 
 myBoard.showGameBoard();
 
 let Player1 = playerFactory("Player 1",""); //I couldn't figure out how to update the mark value when this is a const from the factory function.
-let Player2 = playerFactory("Player 2","");//So i made it a let variable 😧 (yes i used an emoji in code)
+let Player2 = playerFactory("Player 2","");//So i made it a let variable so i can change it later 😧 (yes i used an emoji in code)
+
+
 
 /*
     ################################################
@@ -126,7 +237,6 @@ function getRadioValues() {
         if(getMarkPlayer1.value != getMarkPlayer2.value) {
             Player1 = playerFactory("Player 1", getMarkPlayer1.value);
             Player2 = playerFactory("Player 2", getMarkPlayer2.value);
-            console.log(Player1.getMark());
         } else {
             alert("The selected mark are the same for both players");
         }
